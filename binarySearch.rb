8d582af99ce83e1288f73beba5e2500e24c6bbcb
Brent Cameron <bjcameron@gmail.com>
2021-11-30 17:01:39 -0700

    added findFirstoccurrence in python and js

--- binarySearch.rb
+++ binarySearch.rb
@@ -2,19 +2,21 @@
 
 def binary_search(arr, target)
   leftPointer = 0;
-  rightPointer = arr.length -1 
+  rightPointer = arr.length() -1 
   while(leftPointer <= rightPointer)
-    mid = (leftPointer + rightPointer)/2.foor()
-    if(target > arr[rightPointer])
-      leftPointer = rightPointer;
-      rightPointer = arr.length- 1;
-    elsif (target < arr[rightPointer])
-      rightPointer = rightPointer/2.floor();
-    elsif (target == arr[rightPointer])
-      return rightPointer
+    mid = (leftPointer + rightPointer)/2.floor()
+    if arr[mid] == target 
+      return mid
+    elsif arr[mid] < target 
+      leftPointer = mid + 1
+    else
+      rightPointer = mid -1
+
     end
+
   end
 end
+
 
 arr = [1,3,4,6,7,45,67,86];
 target = 45;
