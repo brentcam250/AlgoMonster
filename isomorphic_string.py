a3c6dd74ea458c33b2f7470c9a66c8cfded67f99
Brent Cameron <bjcameron@gmail.com>
2021-12-02 14:10:31 -0700

    started on isomorphic string

---
+++ isomorphic_string.py
@@ -0,0 +1,14 @@
+def is_isomorphic(str_1: str, str_2: str) -> bool:
+    # WRITE YOUR BRILLIANT CODE HERE
+    if (len(str_1) != len(str_2)):
+        return False
+    else:
+
+
+    return False
+
+if __name__ == '__main__':
+    str_1 = input()
+    str_2 = input()
+    res = is_isomorphic(str_1, str_2)
+    print('true' if res else 'false')
