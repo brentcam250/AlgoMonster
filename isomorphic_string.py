37ec2195ed98ce56a9b5dc2c8b3a7c34b253469d
Brent Cameron <bjcameron@gmail.com>
2021-12-08 12:57:23 -0700

    isomorphic string (Need more practice )

--- isomorphic_string.py
+++ isomorphic_string.py
@@ -1,11 +1,21 @@
 def is_isomorphic(str_1: str, str_2: str) -> bool:
     # WRITE YOUR BRILLIANT CODE HERE
+    mapping = {}
+    used = set()
     if (len(str_1) != len(str_2)):
         return False
-    else:
-
-
-    return False
+    for i in range(len(str_1)):
+      a_char = str_1[i]
+      b_char = str_2[i]
+      if a_char in mapping:
+        if mapping[a_char] != b_char :
+          return False
+        else:
+          if b_char in used:
+            return False
+          mapping[a_char] = b_char
+          used.add(b_char)
+    return True
 
 if __name__ == '__main__':
     str_1 = input()
