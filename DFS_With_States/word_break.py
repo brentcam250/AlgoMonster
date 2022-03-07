c3129334c4914520f358b7350a9bf6323eea07f0
Brent Cameron <bjcameron@gmail.com>
2022-03-07 10:17:03 -0700

    put memoization in

--- DFS_With_States/word_break.py
+++ DFS_With_States/word_break.py
@@ -1,15 +1,22 @@
 from typing import List
 
 def word_break(s: str, words: List[str]) -> bool:
+    memo = {}
     def dfs(i):
         if i == len(s):
             return True 
 
+        if i in memo:
+            return memo[i]
+
+        ok = False
         for word in words: 
             if s[i:].startswith(word):
                 if dfs(i + len(word)):
-                    return True 
-        return False
+                    ok = True 
+                    break
+        memo[i] = ok
+        return ok
     return dfs(0)
 
 if __name__ == '__main__':
