ca0a2f998b0b84421eb92326fba0eb80184e9f9f
Brent Cameron <bjcameron@gmail.com>
2022-03-09 09:10:35 -0700

    level order traversal

---
+++ BFS/level_order_traversal.py
@@ -0,0 +1,38 @@
+from typing import List
+from collections import deque
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def level_order_traversal(root: Node) -> List[List[int]]:
+    # WRITE YOUR BRILLIANT CODE HERE
+    output = []
+    queue = deque([root])
+    while len(queue) > 0:
+        n = len(queue)
+        new_level = []
+        for _ in range(n):
+            node = queue.popleft()
+            new_level.append(node.val)
+            for child in [node.left, node.right]:
+                if child is not None:
+                    queue.append(child)
+        output.append(new_level)
+
+    return output
+
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+if __name__ == '__main__':
+    root = build_tree(iter(input().split()), int)
+    res = level_order_traversal(root)
+    for row in res:
+        print(' '.join(map(str, row)))
